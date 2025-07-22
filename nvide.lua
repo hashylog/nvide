a71809fff2a44fc286a36b72881668831fd8f2dc
hashylog <108955419+hashylog@users.noreply.github.com>
2025-07-22 16:00:56 -0300

    nvide.lua

--- nvide.lua
+++ nvide.lua
@@ -13,8 +13,7 @@
         clipboard integration, fuzzy search,
         and GUI-like UX.
 
-]]
---
+]]--
 
 local Settings = {
   ['Find'] = {
@@ -42,249 +41,247 @@ local Settings = {
 -- Load mswin.vim configuration from VimScript to simulate Windows-like behaviors
 -- mswin.vim
 vim.cmd [[
-
-    " Set options and add mapping such that Vim behaves a lot like MS-Windows
-    "
-    " Maintainer:	The Vim Project <https://github.com/vim/vim>
-    " Last Change:	2024 Mar 13
-    " Former Maintainer:	Bram Moolenaar <Bram@vim.org>
-
-    " Bail out if this isn't wanted.
-    if exists("g:skip_loading_mswin") && g:skip_loading_mswin
-    finish
-    endif
-
-    " set the 'cpoptions' to its Vim default
-    if 1	" only do this when compiled with expression evaluation
-    let s:save_cpo = &cpoptions
-    endif
-    set cpo&vim
-
-    " set 'selection', 'selectmode', 'mousemodel' and 'keymodel' for MS-Windows
-    set selection=exclusive
-    set selectmode=mouse,key
-    set mousemodel=popup
-    set keymodel=startsel,stopsel
-
-    " backspace and cursor keys wrap to previous/next line
-    set backspace=indent,eol,start whichwrap+=<,>,[,]
-
-    " backspace in Visual mode deletes selection
-    vnoremap <BS> "_dd
-
-    " the better solution would be to use has("clipboard_working"),
-    " but that may not be available yet while starting up, so let's just check if
-    " clipboard support has been compiled in and assume it will be working :/
-    if has("clipboard")
-        " CTRL-X and SHIFT-Del are Cut
-        vnoremap <C-X> "+x
-        vnoremap <S-Del> "+x
-
-        " CTRL-C and CTRL-Insert are Copy
-        vnoremap <C-C> "+y
-        vnoremap <C-Insert> "+y
-
-        " CTRL-V and SHIFT-Insert are Paste
-        map <C-V>		"+gP
-        map <S-Insert>		"+gP
-
-        cmap <C-V>		<C-R>+
-        cmap <S-Insert>		<C-R>+
-    else
-            " Use the unnamed register when clipboard support not available
-
-        " CTRL-X and SHIFT-Del are Cut
-        vnoremap <C-X>   x
-        vnoremap <S-Del> x
-
-        " CTRL-C and CTRL-Insert are Copy
-        vnoremap <C-C>      y
-        vnoremap <C-Insert> y
-
-        " CTRL-V and SHIFT-Insert are Paste
-        noremap <C-V>      gP
-        noremap <S-Insert> gP
-
-        inoremap <C-V>      <C-R>"
-        inoremap <S-Insert> <C-R>"
-    endif
-
-    " Pasting blockwise and linewise selections is not possible in Insert and
-    " Visual mode without the +virtualedit feature.  They are pasted as if they
-    " were characterwise instead.
-    " Uses the paste.vim autoload script.
-    " Use CTRL-G u to have CTRL-Z only undo the paste.
-
-    if has("clipboard")
-        exe 'inoremap <script> <C-V> <C-G>u' . paste#paste_cmd['i']
-        exe 'vnoremap <script> <C-V> ' . paste#paste_cmd['v']
-    endif
-
-    imap <S-Insert>		<C-V>
-    vmap <S-Insert>		<C-V>
-
-    " Use CTRL-Q to do what CTRL-V used to do
-    noremap <C-Q>		<C-V>
-
-    " Use CTRL-S for saving, also in Insert mode (<C-O> doesn't work well when
-    " using completions).
-    noremap <C-S>		:update<CR>
-    vnoremap <C-S>		<C-C>:update<CR>
-    inoremap <C-S>		<Esc>:update<CR>
-
-    " For CTRL-V to work autoselect must be off.
-    " On Unix we have two selections, autoselect can be used.
-    if !has("unix")
-    set guioptions-=a
-    endif
-
-    " CTRL-Z is Undo; not in cmdline though
-    noremap <C-Z> u
-    inoremap <C-Z> <C-O>u
-
-    " CTRL-Y is Redo (although not repeat); not in cmdline though
-    noremap <C-Y> <C-R>
-    inoremap <C-Y> <C-O><C-R>
-
-    " Alt-Space is System menu
-    if has("gui")
-    noremap <M-Space> :simalt ~<CR>
-    inoremap <M-Space> <C-O>:simalt ~<CR>
-    cnoremap <M-Space> <C-C>:simalt ~<CR>
-    endif
-
-    " CTRL-A is Select all
-    noremap <C-A> gggH<C-O>G
-    inoremap <C-A> <C-O>gg<C-O>gH<C-O>G
-    cnoremap <C-A> <C-C>gggH<C-O>G
-    onoremap <C-A> <C-C>gggH<C-O>G
-    snoremap <C-A> <C-C>gggH<C-O>G
-    xnoremap <C-A> <C-C>ggVG
-
-    " CTRL-Tab is Next window
-    noremap <C-Tab> <C-W>w
-    inoremap <C-Tab> <C-O><C-W>w
-    cnoremap <C-Tab> <C-C><C-W>w
-    onoremap <C-Tab> <C-C><C-W>w
-
-    " CTRL-F4 is Close window
-    noremap <C-F4> <C-W>c
-    inoremap <C-F4> <C-O><C-W>c
-    cnoremap <C-F4> <C-C><C-W>c
-    onoremap <C-F4> <C-C><C-W>c
-
-    if has("gui")
-    " CTRL-F is the search dialog
-    noremap  <expr> <C-F> has("gui_running") ? ":promptfind\<CR>" : "/"
-    inoremap <expr> <C-F> has("gui_running") ? "\<C-\>\<C-O>:promptfind\<CR>" : "\<C-\>\<C-O>/"
-    cnoremap <expr> <C-F> has("gui_running") ? "\<C-\>\<C-C>:promptfind\<CR>" : "\<C-\>\<C-O>/"
-
-    " CTRL-H is the replace dialog,
-    " but in console, it might be backspace, so don't map it there
-    nnoremap <expr> <C-H> has("gui_running") ? ":promptrepl\<CR>" : "\<C-H>"
-    inoremap <expr> <C-H> has("gui_running") ? "\<C-\>\<C-O>:promptrepl\<CR>" : "\<C-H>"
-    cnoremap <expr> <C-H> has("gui_running") ? "\<C-\>\<C-C>:promptrepl\<CR>" : "\<C-H>"
-    endif
-
-    " restore 'cpoptions'
-    set cpo&
-    if 1
-    let &cpoptions = s:save_cpo
-    unlet s:save_cpo
-    endif
-
+	" Set options and add mapping such that Vim behaves a lot like MS-Windows
+	"
+	" Maintainer:	The Vim Project <https://github.com/vim/vim>
+	" Last Change:	2024 Mar 13
+	" Former Maintainer:	Bram Moolenaar <Bram@vim.org>
+	
+	" Bail out if this isn't wanted.
+	if exists("g:skip_loading_mswin") && g:skip_loading_mswin
+	  finish
+	endif
+	
+	" set the 'cpoptions' to its Vim default
+	if 1	" only do this when compiled with expression evaluation
+	  let s:save_cpo = &cpoptions
+	endif
+	set cpo&vim
+	
+	" set 'selection', 'selectmode', 'mousemodel' and 'keymodel' for MS-Windows
+	" behave mswin
+	" NVide:
+	set selection=exclusive
+	set selectmode=mouse,key
+	set mousemodel=popup
+	set keymodel=startsel,stopsel
+	
+	" backspace and cursor keys wrap to previous/next line
+	set backspace=indent,eol,start whichwrap+=<,>,[,]
+	
+	" backspace in Visual mode deletes selection
+	" NVide: changed 'd' to '"_d'
+	vnoremap <BS> "_d
+	
+	" the better solution would be to use has("clipboard_working"),
+	" but that may not be available yet while starting up, so let's just check if
+	" clipboard support has been compiled in and assume it will be working :/
+	if has("clipboard")
+	    " CTRL-X and SHIFT-Del are Cut
+	    vnoremap <C-X> "+x
+	    vnoremap <S-Del> "+x
+	
+	    " CTRL-C and CTRL-Insert are Copy
+	    vnoremap <C-C> "+y
+	    vnoremap <C-Insert> "+y
+	
+	    " CTRL-V and SHIFT-Insert are Paste
+	    map <C-V>		"+gP
+	    map <S-Insert>		"+gP
+	
+	    cmap <C-V>		<C-R>+
+	    cmap <S-Insert>		<C-R>+
+	else
+			" Use the unnamed register when clipboard support not available
+	
+	    " CTRL-X and SHIFT-Del are Cut
+	    vnoremap <C-X>   x
+	    vnoremap <S-Del> x
+	
+	    " CTRL-C and CTRL-Insert are Copy
+	    vnoremap <C-C>      y
+	    vnoremap <C-Insert> y
+	
+	    " CTRL-V and SHIFT-Insert are Paste
+	    noremap <C-V>      gP
+	    noremap <S-Insert> gP
+	
+	    inoremap <C-V>      <C-R>"
+	    inoremap <S-Insert> <C-R>"
+	endif
+	
+	" Pasting blockwise and linewise selections is not possible in Insert and
+	" Visual mode without the +virtualedit feature.  They are pasted as if they
+	" were characterwise instead.
+	" Uses the paste.vim autoload script.
+	" Use CTRL-G u to have CTRL-Z only undo the paste.
+	
+	if has("clipboard")
+	    exe 'inoremap <script> <C-V> <C-G>u' . paste#paste_cmd['i']
+	    exe 'vnoremap <script> <C-V> ' . paste#paste_cmd['v']
+	endif
+	
+	imap <S-Insert>		<C-V>
+	vmap <S-Insert>		<C-V>
+	
+	" Use CTRL-Q to do what CTRL-V used to do
+	noremap <C-Q>		<C-V>
+	
+	" Use CTRL-S for saving, also in Insert mode (<C-O> doesn't work well when
+	" using completions).
+	noremap <C-S>		:update<CR>
+	vnoremap <C-S>		<C-C>:update<CR>
+	inoremap <C-S>		<Esc>:update<CR>gi
+	
+	" For CTRL-V to work autoselect must be off.
+	" On Unix we have two selections, autoselect can be used.
+	if !has("unix")
+	  set guioptions-=a
+	endif
+	
+	" CTRL-Z is Undo; not in cmdline though
+	noremap <C-Z> u
+	inoremap <C-Z> <C-O>u
+	
+	" CTRL-Y is Redo (although not repeat); not in cmdline though
+	noremap <C-Y> <C-R>
+	inoremap <C-Y> <C-O><C-R>
+	
+	" Alt-Space is System menu
+	if has("gui")
+	  noremap <M-Space> :simalt ~<CR>
+	  inoremap <M-Space> <C-O>:simalt ~<CR>
+	  cnoremap <M-Space> <C-C>:simalt ~<CR>
+	endif
+	
+	" CTRL-A is Select all
+	noremap <C-A> gggH<C-O>G
+	inoremap <C-A> <C-O>gg<C-O>gH<C-O>G
+	cnoremap <C-A> <C-C>gggH<C-O>G
+	onoremap <C-A> <C-C>gggH<C-O>G
+	snoremap <C-A> <C-C>gggH<C-O>G
+	xnoremap <C-A> <C-C>ggVG
+	
+	" CTRL-Tab is Next window
+	noremap <C-Tab> <C-W>w
+	inoremap <C-Tab> <C-O><C-W>w
+	cnoremap <C-Tab> <C-C><C-W>w
+	onoremap <C-Tab> <C-C><C-W>w
+	
+	" CTRL-F4 is Close window
+	noremap <C-F4> <C-W>c
+	inoremap <C-F4> <C-O><C-W>c
+	cnoremap <C-F4> <C-C><C-W>c
+	onoremap <C-F4> <C-C><C-W>c
+	
+	if has("gui")
+	  " CTRL-F is the search dialog
+	  noremap  <expr> <C-F> has("gui_running") ? ":promptfind\<CR>" : "/"
+	  inoremap <expr> <C-F> has("gui_running") ? "\<C-\>\<C-O>:promptfind\<CR>" : "\<C-\>\<C-O>/"
+	  cnoremap <expr> <C-F> has("gui_running") ? "\<C-\>\<C-C>:promptfind\<CR>" : "\<C-\>\<C-O>/"
+	
+	  " CTRL-H is the replace dialog,
+	  " but in console, it might be backspace, so don't map it there
+	  nnoremap <expr> <C-H> has("gui_running") ? ":promptrepl\<CR>" : "\<C-H>"
+	  inoremap <expr> <C-H> has("gui_running") ? "\<C-\>\<C-O>:promptrepl\<CR>" : "\<C-H>"
+	  cnoremap <expr> <C-H> has("gui_running") ? "\<C-\>\<C-C>:promptrepl\<CR>" : "\<C-H>"
+	endif
+	
+	" restore 'cpoptions'
+	set cpo&
+	if 1
+	  let &cpoptions = s:save_cpo
+	  unlet s:save_cpo
+	endif
 ]]
 
 -- Load evim.vim script to configure a more beginner-friendly GUI experience
 -- evim.vim
 vim.cmd [[
-    " Vim script for Evim key bindings
-    " Maintainer:	The Vim Project <https://github.com/vim/vim>
-    " Last Change:	2023 Aug 10
-    " Former Maintainer:	Bram Moolenaar <Bram@vim.org>
-
-    " Don't use Vi-compatible mode.
-    set nocompatible
-
-    " Use the mswin.vim script for most mappings
-    " source <sfile>:p:h/mswin.vim
-
-	" NVide:
-    " Allow for using CTRL-Q in Insert mode to quit Vim.
-    inoremap <C-Q> <C-O>:confirm q<CR>
-	" Allow for using CTRL-Q in Normal Mode to quit Vim.
-    nnoremap <C-Q> :confirm q<CR>
-    " Allow for using CTRL-Q in Visual Mode to quit Vim.
-    vnoremap <C-Q> <Esc>:confirm q<CR>
-    " Allow for using CTRL-Q in Command Mode to quit Vim.
-    cnoremap <C-Q> <C-C>:confirm q<CR>
-
-    " Vim is in Insert mode by default
-    startinsert
-
-    " Make a buffer hidden when editing another one
-    set hidden
-
-    " Make cursor keys ignore wrapping
-    inoremap <silent> <Down> <C-R>=pumvisible() ? "\<lt>Down>" : "\<lt>C-O>gj"<CR>
-    inoremap <silent> <Up> <C-R>=pumvisible() ? "\<lt>Up>" : "\<lt>C-O>gk"<CR>
-
-    " CTRL-F does Find dialog instead of page forward
-    " noremap <C-F> /
-    " vnoremap <C-F> y/
-    " onoremap <C-F> <C-C>/
-    " inoremap <C-F> <C-O>/
-    " cnoremap <C-F> <C-C>/
-
-    set backspace=2		" allow backspacing over everything in insert mode
-    set autoindent		" always set autoindenting on
-    if has("vms")
-    set nobackup		" do not keep a backup file, use versions instead
-    else
-    set backup		" keep a backup file
-    endif
-    set history=50		" keep 50 lines of command line history
-    set ruler		" show the cursor position all the time
-    set incsearch		" do incremental searching
-    set mouse=a		" always use the mouse
-
-    " Don't use Ex mode, use Q for formatting
-    map Q gq
-
-    " Switch syntax highlighting on, when the terminal has colors
-    " Highlight the last used search pattern on the next search command.
-    if &t_Co > 2 || has("gui_running")
-    syntax on
-    set hlsearch
-    nohlsearch
-    endif
-
-    " Enable file type detection.
-    " Use the default filetype settings, so that mail gets 'tw' set to 72,
-    " 'cindent' is on in C files, etc.
-    " Also load indent files, to automatically do language-dependent indenting.
-    filetype plugin indent on
-
-    " For all text files set 'textwidth' to 78 characters.
-    au FileType text setlocal tw=78
-
-    " Add optional packages.
-    "
-    " The matchit plugin makes the % command work better, but it is not backwards
-    " compatible.
-    " The ! means the package won't be loaded right away but when plugins are
-    " loaded during initialization.
-    if has('syntax') && has('eval')
-    packadd! matchit
-    endif
-
-    " vim: set sw=2 :
+	" Vim script for Evim key bindings
+	" Maintainer:	The Vim Project <https://github.com/vim/vim>
+	" Last Change:	2023 Aug 10
+	" Former Maintainer:	Bram Moolenaar <Bram@vim.org>
+	
+	" Don't use Vi-compatible mode.
+	set nocompatible
+	
+	" Use the mswin.vim script for most mappings
+	" NVide: Removed since the script is loaded above
+	" source <sfile>:p:h/mswin.vim
+	
+	" Allow for using CTRL-Q in Insert mode to quit Vim.
+	inoremap <C-Q> <C-O>:confirm qall<CR>
+	
+	" Vim is in Insert mode by default
+	" NVide: Changed "set insertmode" to "startinsert"
+	" set insertmode
+	startinsert
+	
+	" Make a buffer hidden when editing another one
+	set hidden
+	
+	" Make cursor keys ignore wrapping
+	inoremap <silent> <Down> <C-R>=pumvisible() ? "\<lt>Down>" : "\<lt>C-O>gj"<CR>
+	inoremap <silent> <Up> <C-R>=pumvisible() ? "\<lt>Up>" : "\<lt>C-O>gk"<CR>
+	
+	" CTRL-F does Find dialog instead of page forward
+	noremap <silent> <C-F> :promptfind<CR>
+	vnoremap <silent> <C-F> y:promptfind <C-R>"<CR>
+	onoremap <silent> <C-F> <C-C>:promptfind<CR>
+	inoremap <silent> <C-F> <C-O>:promptfind<CR>
+	cnoremap <silent> <C-F> <C-C>:promptfind<CR>
+	
+	
+	set backspace=2		" allow backspacing over everything in insert mode
+	set autoindent		" always set autoindenting on
+	if has("vms")
+	  set nobackup		" do not keep a backup file, use versions instead
+	else
+	  set backup		" keep a backup file
+	endif
+	set history=50		" keep 50 lines of command line history
+	set ruler		" show the cursor position all the time
+	set incsearch		" do incremental searching
+	set mouse=a		" always use the mouse
+	
+	" Don't use Ex mode, use Q for formatting
+	map Q gq
+	
+	" Switch syntax highlighting on, when the terminal has colors
+	" Highlight the last used search pattern on the next search command.
+	if &t_Co > 2 || has("gui_running")
+	  syntax on
+	  set hlsearch
+	  nohlsearch
+	endif
+	
+	" Enable file type detection.
+	" Use the default filetype settings, so that mail gets 'tw' set to 72,
+	" 'cindent' is on in C files, etc.
+	" Also load indent files, to automatically do language-dependent indenting.
+	filetype plugin indent on
+	
+	" For all text files set 'textwidth' to 78 characters.
+	au FileType text setlocal tw=78
+	
+	" Add optional packages.
+	"
+	" The matchit plugin makes the % command work better, but it is not backwards
+	" compatible.
+	" The ! means the package won't be loaded right away but when plugins are
+	" loaded during initialization.
+	if has('syntax') && has('eval')
+	  packadd! matchit
+	endif
+	
+	" vim: set sw=2 :
 ]]
 
 -- Prevent leaving Insert Mode except via [Ctrl + E]
 _G.nvide_normal_mode = false
 
--- Always enter Insert Mode when entering a buffer
+-- Always enter Insert Mode when entering a buffer when it's modifiable
 vim.api.nvim_create_autocmd('BufEnter', {
   pattern = '*',
   callback = function()
@@ -300,6 +297,7 @@ vim.api.nvim_create_autocmd('BufEnter', {
   end,
 })
 
+-- Enter Normal Mode
 vim.keymap.set('i', '<C-E>', function()
   _G.nvide_normal_mode = true
   vim.cmd 'stopinsert'
@@ -320,8 +318,7 @@ vim.api.nvim_create_autocmd('ModeChanged', {
   end,
 })
 
-vim.api.nvim_set_keymap('i', '<Esc>', '<NOP>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<C-c>', '<NOP>', { noremap = true })
+
 
 -- Fuzzy finder in current buffer [Ctrl + F]
 if Settings['Find']['Enabled'] then
@@ -342,6 +339,8 @@ if Settings['Find']['Enabled'] then
   end
 end
 
+
+
 -- Fuzzy finder for files [Ctrl + P]
 if Settings['FindFiles']['Enabled'] then
   if pcall(require, 'telescope.builtin') then
@@ -351,6 +350,8 @@ if Settings['FindFiles']['Enabled'] then
   end
 end
 
+
+
 -- Open integrated terminal [Ctrl + T]
 if Settings['Terminal']['Enabled'] then
   vim.keymap.set({ 'n', 'i' }, Settings['Terminal']['Shortcut'], function()
@@ -358,6 +359,8 @@ if Settings['Terminal']['Enabled'] then
     vim.cmd 'startinsert'
   end, { noremap = true, silent = true })
 end
+
+
 
 -- Go to line [Ctrl + G]
 local function goto_line()
@@ -379,24 +382,7 @@ if Settings['GoToLine']['Enabled'] then
   vim.keymap.set({ 'n', 'i' }, Settings['GoToLine']['Shortcut'], goto_line, { noremap = true, silent = true })
 end
 
--- Go to definition [Ctrl + T] [F12]
 
---[[ 
-if pcall(require, 'telescope.builtin') then
-  
-  vim.keymap.set({ 'n', 'i' }, '<C-T>', function()
-    require('telescope.builtin').lsp_document_symbols()
-  end, { noremap = true, silent = true })
-  
-  vim.keymap.set({ 'n', 'i' }, '<F12>', function()
-    if vim.lsp.buf and vim.lsp.buf.definition then
-      vim.lsp.buf.definition()
-    end
-  end, { noremap = true, silent = true })
-  
-end
-]]
---
 
 -- Move lines up/down with Alt+Up/Down
 vim.keymap.set('i', '<M-Up>', '<Esc>:m .-2<CR>==gi', { noremap = true, silent = true })
@@ -406,11 +392,17 @@ vim.keymap.set('n', '<M-Down>', ':m .+1<CR>==', { noremap = true, silent = true 
 vim.keymap.set('v', '<M-Up>', ":move '<-2<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set('v', '<M-Down>', ":move '>+1<CR>gv=gv", { noremap = true, silent = true })
 
+
+
 -- TODO: Multi-line indentation in Visual Mode [Tab]
 -- ...
 
+
+
 -- Delete previous word in Insert Mode [Ctrl + Backspace]
 vim.keymap.set('i', '<C-BS>', '<C-W>', { noremap = true })
+
+
 
 -- File explorer sidebar [Ctrl + B] (Requires 'neo-tree' plugin)
 local function toggle_neotree()
