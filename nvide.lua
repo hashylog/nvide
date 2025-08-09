b11096c97ae42f76ee47c7ec8be9ac4204d6c3ad
hashylog <108955419+hashylog@users.noreply.github.com>
2025-08-09 03:36:58 -0300

    nvide.lua

--- nvide.lua
+++ nvide.lua
@@ -131,7 +131,7 @@ vim.cmd [[
 	" using completions).
 	noremap <C-S>		:update<CR>
 	vnoremap <C-S>		<C-C>:update<CR>
-	inoremap <C-S>		<Esc>:update<CR>gi
+	inoremap <C-S>		<Esc>:update<CR>
 	
 	" For CTRL-V to work autoselect must be off.
 	" On Unix we have two selections, autoselect can be used.
